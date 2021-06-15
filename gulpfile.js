e51769049b0112a253dde3d250be46e7fa293c4c
Fabian Gutierrez <fega.hg@gmail.com>
2021-06-14 22:28:42 -0500

    Actualizado gulp y otras librerias

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,8 @@ var gulp = require('gulp');
 var hb = require('gulp-hb');
 var csso = require('gulp-csso');
 var concat = require('gulp-concat');
-var csslint = require('gulp-csslint');
 var browserSync = require('browser-sync');
 var uglify = require('gulp-uglify');
-var jshint = require('gulp-jshint');
 //var svgmin = require('gulp-svgmin');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
@@ -68,24 +66,12 @@ gulp.task('build',   function () {
         .pipe(gulp.dest('./dist'))
         //.pipe(browserSync.reload({ stream: false }));
 });
-gulp.task('build-watch', ['build'], browserSync.reload);
+gulp.task('build-watch', gulp.series('build', browserSync.reload));
 gulp.task('deploy', function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages({'branch':'master', 'remoteUrl':'http://github.com/gps-tracker/gps-tracker.github.io.git'}));
 });
-/*///////////////////////////////////////
-linters
-///////////////////////////////////////*/
-gulp.task('jslint', function() {
-  return gulp.src('./src/js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-gulp.task('csslint', function() {
-  gulp.src(['./src/css/style.css'])
-    .pipe(csslint())
-    .pipe(csslint.reporter('compact'));
-});
+
 /*///////////////////////////////////////
 JS watcher
 ///////////////////////////////////////*/
@@ -161,12 +147,12 @@ gulp.task('root', function () {
 /*///////////////////////////////////////
 WATCHER
 ///////////////////////////////////////*/
-gulp.task('default', ['browser-sync','js','css','build','svg','img','root'], function () {
-	gulp.watch(['./src/js/*.js'],   ['js','jslint']);
-	gulp.watch('./src/**/*.css',  ['css','csslint']);
-	gulp.watch('./src/root/*',  ['browser-sync','root','css-root']);
+gulp.task('default', gulp.parallel('browser-sync','js','css','build','svg','img','root', ()=>  {
+	gulp.watch('./src/js/*.js',   gulp.series(['js']));
+	gulp.watch('./src/**/*.css',  gulp.series(['css']));
+	gulp.watch('./src/root/*',  gulp.series(['browser-sync','root','css-root']));
 	//gulp.watch('./dist/**/*.html', ['bs-reload']);
-	gulp.watch(['./src/views/**/*'], ['build-watch']);
-	gulp.watch('./src/images/**/*.svg', ['svg']);
-	gulp.watch('src/**/*.+(png|jpg|jpeg|gif)', ['img']);
-});
+	gulp.watch(['./src/views/**/*'], gulp.series(['build-watch']));
+	gulp.watch('./src/images/**/*.svg', gulp.series(['svg']));
+	gulp.watch('src/**/*.+(png|jpg|jpeg|gif)', gulp.series(['img']));
+}));
